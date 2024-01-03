51e2f9708f179a0dce74ab7dee5e122a880c90d9
Documenter.jl <documenter@juliadocs.github.io>
2024-01-03 22:16:49 +0000

    build based on 05974e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VLBIDataDList","category":"page"},{"location":"#VLBIDataDList","page":"Home","title":"VLBIDataDList","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VLBIDataDList.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VLBIDataDList]","category":"page"}]
+}
